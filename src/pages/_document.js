e2b43e5fd488e88c450a565f590011e1684c9946
rafael <rafael211295@gmail.com>
2026-02-19 12:07:10 -0300

    insert meta and google pixel

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,5 +1,5 @@
-"use client";
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
@@ -33,9 +33,42 @@ export default class MyDocument extends Document {
     return (
       <Html lang="pt-BR">
         <Head>
+          <title>Bruttus Distribuidora</title>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
+          <link rel="icon" href="/bruttusbg.ico" />
+          
+          {/* Google Ads Conversion Tracking */}
+          <Script async src="https://www.googletagmanager.com/gtag/js?id=AW-17940498068" strategy="afterInteractive" />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-17940498068');`}
+          </Script>
+
+          {/* Meta Pixel Code */}
+          <Script id="facebook-pixel" strategy="afterInteractive">
+            {`!function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1276599831003778');
+              fbq('track', 'PageView');`}
+          </Script>
+          <noscript>
+            <img
+              height="1"
+              width="1"
+              style={{ display: 'none' }}
+              src="https://www.facebook.com/tr?id=1276599831003778&ev=PageView&noscript=1"
+            />
+          </noscript>
         </Head>
         <body>
           <Main />
